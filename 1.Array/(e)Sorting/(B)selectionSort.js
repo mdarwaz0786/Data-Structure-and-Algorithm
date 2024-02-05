a7a9775b9b8f9dd547c977b6751d38de25184cbc
Md Arwaz <mdarwaznew2017@gmail.com>
2024-02-05 14:54:38 +0530

    write code for selection sort

--- 1.Array/(e)Sorting/(B)selectionSort.js
+++ 1.Array/(e)Sorting/(B)selectionSort.js
@@ -1,13 +1,15 @@
 // sort array element using selection sort algorithm
 function selectionSort(array) {
   for (let i = 0; i < array.length - 1; i++) {
-    let minIndex = i;
+    let min = i;
     for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < array[minIndex]) {
-        minIndex = j;
+      if (array[j] < array[min]) {
+        min = j;
       };
     };
-    [array[i], array[minIndex]] = [array[minIndex], array[i]];
+    if (min != i) {
+      [array[i], array[min]] = [array[min], array[i]];
+    };
   };
   return array;
 };
