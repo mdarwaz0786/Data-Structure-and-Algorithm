4016895d0e70915b3c83dc01d7f800d3780cf052
Md Arwaz <mdarwaznew2017@gmail.com>
2024-02-25 16:23:33 +0530

    union of two array

---
+++ 1.Array/(g)Question/union.js
@@ -0,0 +1,12 @@
+function unionArray(array1, array2) {
+  const set1 = new Set(array1);
+  const set2 = new Set(array2);
+  const result = [...new Set([...set1, ...set2])];
+  return result;
+};
+
+const union = unionArray([1, 2, 2, 2, 3], [2, 3, 3, 4, 5, 5]);
+console.log(union);
+
+// Output:
+// [ 1, 2, 3, 4, 5 ]
